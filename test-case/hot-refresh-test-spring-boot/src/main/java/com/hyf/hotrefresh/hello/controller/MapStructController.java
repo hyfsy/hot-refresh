de2a229f6849aa001aab3b461ef009d7f6cc633f
hyfsy <1577975140@qq.com>
2022-05-14 01:08:00 +0800

    fix(bug): fix #I56MFU

--- test-case/hot-refresh-test-spring-boot/src/main/java/com/hyf/hotrefresh/hello/controller/MapStructController.java
+++ test-case/hot-refresh-test-spring-boot/src/main/java/com/hyf/hotrefresh/hello/controller/MapStructController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test/mapstruct")
 public class MapStructController {
 
+
     @RequestMapping("1")
     public void _1() {
     }
