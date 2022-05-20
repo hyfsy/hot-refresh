5a3bca1dc7e6710e534fbc6d264c3876ae25aec0
hyfeng <hyfeng@epoint.com.cn>
2022-05-20 22:59:41 +0800

    fix(client): fixed client can load META-INF/services/ file in plugin directory and some bugs.
    (cherry picked from commit 0feadc0bd031445ae04dbe0ead286dc3858ceade)

--- hot-refresh-core/src/main/java/com/hyf/hotrefresh/core/remoting/handler/RpcHotRefreshResponseHandler.java
+++ hot-refresh-core/src/main/java/com/hyf/hotrefresh/core/remoting/handler/RpcHotRefreshResponseHandler.java
@@ -1,15 +1,11 @@
 package com.hyf.hotrefresh.core.remoting.handler;
 
-import com.hyf.hotrefresh.core.remoting.payload.RpcHotRefreshResponse;
-import com.hyf.hotrefresh.remoting.rpc.handler.RpcMessageResponseHandler;
+import com.hyf.hotrefresh.remoting.rpc.handler.RpcResponseHandler;
 
 /**
  * @author baB_hyf
  * @date 2022/05/19
  */
-public class RpcHotRefreshResponseHandler extends RpcMessageResponseHandler<RpcHotRefreshResponse> {
+public class RpcHotRefreshResponseHandler extends RpcResponseHandler {
 
-    @Override
-    public void handleResponse(RpcHotRefreshResponse request) throws Exception {
-    }
 }
