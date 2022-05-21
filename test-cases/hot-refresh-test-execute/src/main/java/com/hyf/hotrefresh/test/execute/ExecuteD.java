2fe8714d561465c6f00739962683894cb370f56d
hyfeng <hyfeng@epoint.com.cn>
2022-05-21 11:57:37 +0800

    test(execute): add test case

---
+++ test-cases/hot-refresh-test-execute/src/main/java/com/hyf/hotrefresh/test/execute/ExecuteD.java
@@ -0,0 +1,12 @@
+package com.hyf.hotrefresh.test.execute;
+
+/**
+ * @author baB_hyf
+ * @date 2022/05/21
+ */
+public class ExecuteD {
+
+    public static void main(String[] args) {
+        throw new RuntimeException("test exception");
+    }
+}
