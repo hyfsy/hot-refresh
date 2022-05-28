82271f447ad981001a71172bc429ae449306cd4c
hyfsy <1577975140@qq.com>
2022-05-28 17:48:49 +0800

    docs(refresh): Uncompatibility with skywalking

--- test-cases/hot-refresh-test-spring-boot/src/main/java/com/hyf/hotrefresh/hello/TestHotRefreshApplication.java
+++ test-cases/hot-refresh-test-spring-boot/src/main/java/com/hyf/hotrefresh/hello/TestHotRefreshApplication.java
@@ -3,6 +3,11 @@ package com.hyf.hotrefresh.hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 与skywalking不兼容：
+ * <p>
+ * -javaagent:E:\study\env\component\cloud-resources\skywalking\apache-skywalking-apm-8.5.0\agent\skywalking-agent.jar -Dskywalking.agent.service_name=test-hot-refresh-application -Dskywalking.plugin.jdbc.trace_sql_parameters=true
+ */
 @SpringBootApplication
 public class TestHotRefreshApplication {
 
