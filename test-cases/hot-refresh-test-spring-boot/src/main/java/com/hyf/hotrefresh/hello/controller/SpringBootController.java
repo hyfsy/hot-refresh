8205f9ddf1e803c5bd4547359b756e8efca63359
hyfsy <1577975140@qq.com>
2022-05-28 15:42:42 +0800

    test(springboot): Add spring boot test case.

--- test-cases/hot-refresh-test-spring-boot/src/main/java/com/hyf/hotrefresh/hello/controller/SpringBootController.java
+++ test-cases/hot-refresh-test-spring-boot/src/main/java/com/hyf/hotrefresh/hello/controller/SpringBootController.java
@@ -1,5 +1,7 @@
 package com.hyf.hotrefresh.hello.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("test/springboot")
 public class SpringBootController {
 
+    // @Value("${server.port}")
+    // private Integer port;
+
+    @Autowired
+    private ApplicationContext context;
+
     @RequestMapping("1")
     public void _1() {
+        // System.out.println(port);
+        System.out.println(context);
     }
+
+    // @RequestMapping("2")
+    // public boolean _2() {
+    //     return true;
+    // }
 }
