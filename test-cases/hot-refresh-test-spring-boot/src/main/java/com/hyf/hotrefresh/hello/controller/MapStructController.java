e393fa4d23e565edf61097d779e0bc6913f5e090
hyfsy <1577975140@qq.com>
2022-06-25 13:13:51 +0800

    test(lombok): add lombok mapstruct mixed test case

--- test-cases/hot-refresh-test-spring-boot/src/main/java/com/hyf/hotrefresh/hello/controller/MapStructController.java
+++ test-cases/hot-refresh-test-spring-boot/src/main/java/com/hyf/hotrefresh/hello/controller/MapStructController.java
@@ -1,6 +1,7 @@
 package com.hyf.hotrefresh.hello.controller;
 
 import com.hyf.hotrefresh.hello.convert.HelloConverter;
+import com.hyf.hotrefresh.hello.convert.MixedConverter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,12 @@ public class MapStructController {
     @RequestMapping("1")
     public boolean addCompiledMethod() {
         HelloConverter converter = HelloConverter.INSTANCE;
+        return converter.getClass().getDeclaredMethods().length > 0;
+    }
+
+    @RequestMapping("2")
+    public boolean mixCompiledMethod() {
+        MixedConverter converter = MixedConverter.INSTANCE;
         return converter.getClass().getDeclaredMethods().length > 0;
     }
 }
