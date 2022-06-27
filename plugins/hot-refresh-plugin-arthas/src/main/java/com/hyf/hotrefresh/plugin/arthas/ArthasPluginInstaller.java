d68b40dfa3240780e6b12967beb43b7b6ef242d5
hyfsy <1577975140@qq.com>
2022-06-27 16:54:12 +0800

    feat(plugins): arthas feature temporary storage

--- plugins/hot-refresh-plugin-arthas/src/main/java/com/hyf/hotrefresh/plugin/arthas/ArthasPluginInstaller.java
+++ plugins/hot-refresh-plugin-arthas/src/main/java/com/hyf/hotrefresh/plugin/arthas/ArthasPluginInstaller.java
@@ -1,10 +1,8 @@
 package com.hyf.hotrefresh.plugin.arthas;
 
+import com.hyf.hotrefresh.common.util.ReflectionUtils;
 import com.hyf.hotrefresh.core.install.Installer;
-import com.taobao.arthas.agent.attach.ArthasAgent;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.hyf.hotrefresh.core.util.Util;
 
 /**
  * @author baB_hyf
@@ -21,6 +19,11 @@ public class ArthasPluginInstaller implements Installer {
 
         // Map<String, String> params = new HashMap<>();
         // ArthasAgent.attach(params);
-        ArthasAgent.attach();
+        try {
+            Class<?> arthasAgentClass = ReflectionUtils.forName("com.taobao.arthas.agent.attach.ArthasAgent",
+                Util.getInfrastructureJarClassLoader());
+            ReflectionUtils.getMethod(arthasAgentClass, "attach");
+        } catch (Exception ignored) {
+        }
     }
 }
