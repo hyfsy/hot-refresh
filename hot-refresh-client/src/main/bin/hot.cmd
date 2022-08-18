03015ecac9908b1f1bbf0914580b6325ec167c46
hyfsy <1577975140@qq.com>
2022-08-18 19:16:37 +0800

    build(core): directory rename
    (cherry picked from commit 927e6c2a5495b5949df549272ac9b7dac921d235)

--- hot-refresh-client/src/main/bin/hot.cmd
+++
@@ -1,2 +0,0 @@
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -jar E:\hot\lib\hot-refresh-client-1.1.0-SNAPSHOT.jar %*
-rem  -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
